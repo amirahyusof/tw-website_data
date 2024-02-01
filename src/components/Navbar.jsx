c243d1f3f9c712a938a7608144bbe7334572d2fa
amirah <mirez.data@gmail.com>
2024-02-01 22:55:39 +0800

    commit changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="text-white">
+            <h1 className="w-full">DataFinance</h1>
+        </div>
+    )
+}
+
+export default Navbar
