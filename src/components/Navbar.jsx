9e22cb6c9a22c9f9d189ca01f7cb0d3f456d982c
amirah <mirez.data@gmail.com>
2024-02-01 23:05:13 +0800

    commit changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div className="text-white">
-            <h1 className="w-full">DataFinance</h1>
+        <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-3 text-white">
+            <h1 className="w-full text-2xl font-bold text-[#030637]">DataFinance</h1>
+            <ul className="flex">
+                <li className="p-3">Home</li>
+                <li className="p-3">Company</li>
+                <li className="p-3">Resources</li>
+                <li className="p-3">About</li>
+                <li className="p-3">Contact</li>
+            </ul>
         </div>
     )
 }
