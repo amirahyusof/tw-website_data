178ad0b8661deec616b7345dddd651cf7c6de096
amirah <mirez.data@gmail.com>
2024-02-04 23:47:42 +0800

    commit changes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { ReactTyped } from "react-typed";
+
+const Hero =() => {
+    return(
+        <div className="text-white">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+                <p className="text-[#3C0753] font-bold p-2">Transform your financial journey with our data-driven insights</p>
+                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Unleash the Power of Data for Financial Mastery</h1>
+                <div className="flex justify-center items-center">
+                    <p className="md:text-5xl sm:text-4sml text-xl font-bold pt-4">Fast, flexible financing for</p>
+                    <ReactTyped 
+                     className="md:text-5xl sm:text-4sml text-xl font-bold pt-4 pl-2"
+                    strings={[
+                        'PA',
+                        'PFD',
+                        'RMS'
+                    ]}
+                    typeSpeed={120}
+                    backSpeed={140} 
+                    loop/>
+
+                </div>
+                <p className="md:text-xl text-l font-bold text-gray-400 pt-4" >Join the data revolution and transform your financial landscape today. Explore our platform and discover how data can revolutionize your financial future.</p>
+                <button className="bg-[#3C0753] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Get Started</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react"; 
+import React from "react"; 
 import {RiMenu3Fill, RiCloseFill } from 'react-icons/ri';
 
 
 const Navbar = () => {
-    const [nav, setNav] = React.useState(false)
+    const [nav, setNav] = React.useState(true)
 
     const handleNav =() => {
         setNav(!nav)
@@ -12,14 +12,14 @@ const Navbar = () => {
     return (
         <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-3 text-white">
             <h1 className="w-full text-2xl font-bold text-[#030637]">DataFinance</h1>
-            <ul className="flex hidden">
+            <ul className="hidden md:flex">
                 <li className="p-3">Home</li>
                 <li className="p-3">Company</li>
                 <li className="p-3">Resources</li>
                 <li className="p-3">About</li>
                 <li className="p-3">Contact</li>
             </ul>
-            <div onClick={handleNav}>
+            <div onClick={handleNav} className="block md:hidden">
                 {!nav ? <RiCloseFill size={23} /> : <RiMenu3Fill size={23} /> }
                 
             </div>
